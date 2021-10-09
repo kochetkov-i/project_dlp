4f5220fc68ceb56a145a55b4315d7516b5c7a354
kochetkov-i <64097358+kochetkov-i@users.noreply.github.com>
2021-10-09 17:08:19 +0300

    Delete config.py

--- webapp/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    DEBUG = True
-    TESTING = False
-    SQLALCHEMY_DATABASE_URI = ''
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = ''
-    FLASK_APP = 'webapp'
-    FLASK_ADMIN_SWATCH = 'cerulean'
